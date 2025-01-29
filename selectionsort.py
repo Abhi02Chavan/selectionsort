11f753772735459d06c6964c093ed58735e5d4b8
Abhi02Chavan <abhi02chavan@gmail.com>
2025-01-29 23:40:55 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,22 @@
+def selectionsort(array):
+    for i in range (len(array)):
+        for j in range(len(array)):
+            if array[j] < array[i]:
+                array[i],array[j]=array[j],array[i]
+    return array
+                
+            
+# size=int(input("enter the size of array: "))
+# array=[]
+# while size !=0:
+#     element=input("enter your elemnt: ")
+#     array.append(element)
+#     size = size-1
+# print(f"Your entered array is : {array}")
+array=[23,21,43,56,12,32]
+sortedarray=selectionsort(array)
+arrayt=int(reversed(sortedarray))
+print(arrayt)
+# str="hello"
+# # print(str[::-1])
+# print(str[::2])
