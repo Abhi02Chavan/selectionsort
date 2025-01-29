ccd5b8e2a1ce801dc9429f63dd6fa79f4345034c
Abhi02Chavan <abhi02chavan@gmail.com>
2025-01-29 23:46:44 +0530

    selectionsort.py

--- selectionsort.py
+++ selectionsort.py
@@ -6,14 +6,14 @@ def selectionsort(array):
     return array
                 
             
-# size=int(input("enter the size of array: "))
-# array=[]
-# while size !=0:
-#     element=input("enter your elemnt: ")
-#     array.append(element)
-#     size = size-1
-# print(f"Your entered array is : {array}")
-array=[23,21,43,56,12,32]
+size=int(input("enter the size of array: "))
+array=[]
+while size !=0:
+    element=input("enter your elemnt: ")
+    array.append(element)
+    size = size-1
+print(f"Your entered array is : {array}")
+# array=[23,21,43,56,12,32]
 sortedarray=selectionsort(array)
 arrayt=int(reversed(sortedarray))
 print(arrayt)
